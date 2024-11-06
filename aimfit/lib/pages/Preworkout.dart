85a6a3505edd5fba675cfee44ba34385e7010ff2
SalmaDorgham <2211146689@ku.edu.kw>
2024-11-06 10:47:55 +0300

    pages

---
+++ aimfit/lib/pages/Preworkout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Preworkout extends StatelessWidget {
+  const Preworkout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
