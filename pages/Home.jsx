e650b8c1c1fb8bbe190a0c5a30eb9318324133fe
ZahraaEzzat <s2201131107@sci.ku.edu.kw>
2024-12-12 16:53:13 +0300

    homepage

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,16 +3,25 @@ import logo from '../assets/logo.png';
 import Profile_pic from '../assets/profile_page.png';
 
 
-const Home = ({navigateToProfile})=> {
+const Home = ({navigateToProfile, navigateToPreWorkout})=> {
     return(
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Image source={logo} style={styles.logo} />
         <Text style={styles.title}>Home page</Text>
         <TouchableOpacity onPress={navigateToProfile}>
-        <Image source={Profile_pic} style={styles.profile} />
+            <Image source={Profile_pic} style={styles.profile} />
         </TouchableOpacity>
+        <Text style={styles.texttext}>What are you up to?</Text>
       </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={navigateToPreWorkout}>
+            <Text style={styles.buttonText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={navigateToProfile}>
+            <Text style={styles.buttonText}>Check workouts reports</Text>
+        </TouchableOpacity>
+    </View>
     </SafeAreaView>
     );
 };
@@ -29,19 +38,44 @@ const styles = StyleSheet.create({
   logo: {
     width: 200,
     height: 100,
+    alignItems: 'center',
   },
   title:{
     fontSize: 34,
     fontWeight: 'bold',
     color: '#fff',
-    fontWeight: 'bold',
   },
   profile: {
     width: 100,
     height: 100,
     borderRadius: 50,
     marginBottom: 20,
-},
+    marginTop: 20,
+  },
+  texttext: {
+    fontSize: 31,
+    color: '#fff',
+  },
+  buttonContainer: {
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  button: {
+    backgroundColor: '#D32F2F',
+    borderRadius: 25,
+    paddingVertical: 15,
+    marginVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 130,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 27,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
 });
 
 
