c5da7c98917d95126ec69773f31690822d4f520d
almashooq <aidaalmashooq@outlook.com>
2025-02-27 12:20:51 +0300

    updates

---
+++ android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/Recent.kt
@@ -0,0 +1,15 @@
+package com.google.mediapipe.examples.poselandmarker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.mediapipe.examples.poselandmarker.databinding.ActivityRecentBinding
+
+class RecentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRecentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
